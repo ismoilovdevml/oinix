a5b465c07881a0e04ecb2539d137bffcb2a1632f
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2022-07-09 03:22:54 +0500

    updated oinix

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="oinix"
-iso_label="OINIX_$(date +%Y%m)"
-iso_publisher="Ismoilovdev <https://github.com/ismoilovdevml>"
-iso_application="Minimal Arch based Distiro community Xinux  Uzbekistan "
+iso_name="OINIX"
+iso_label="archcraft_$(date +%Y%m)"
+iso_publisher="Otabek Ismoilov <http://www.github.com/ismoilovdevml>"
+iso_application="Minimal linux distribution created by ismoilovdev & Xinux Uzbekistan"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
@@ -14,12 +14,11 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/etc/shadow"]="0:0:0400"
   ["/etc/gshadow"]="0:0:0400"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/customize_airootfs.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
 )
